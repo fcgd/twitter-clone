aa0232ebdbf152e056eafe907a18198b3a907c98
Fabricio C <fabriciocesaretti@gmail.com>
2022-09-15 17:29:09 -0300

    Edit name, username and bio

--- pages/[username].jsx
+++ pages/[username].jsx
@@ -6,15 +6,17 @@ import Cover from "../components/Cover";
 import Layout from "../components/Layout";
 import PostContent from "../components/PostContent";
 import TopNavLink from "../components/TopNavLink";
+import useUserInfo from "../hooks/useUserInfo";
 
 export default function UserPage() {
   const router = useRouter();
   const { username } = router.query;
   const [profileInfo, setProfileInfo] = useState();
+  const [originalUserInfo, setOriginalUserInfo] = useState();
+  const { userInfo } = useUserInfo();
   const [posts, setPosts] = useState([]);
   const [postsLikedByMe, setPostsLikedByMe] = useState([]);
-  // const [editMode,setEditMode] = useState(false);
-  // const [isFollowing,setIsFollowing] = useState(false);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     if (!username) {
@@ -22,6 +24,7 @@ export default function UserPage() {
     }
     axios.get("/api/users?username=" + username).then((response) => {
       setProfileInfo(response.data.user);
+      setOriginalUserInfo(response.data.user);
     });
   }, [username]);
 
@@ -39,6 +42,26 @@ export default function UserPage() {
     setProfileInfo((prev) => ({ ...prev, [type]: src }));
   }
 
+  async function updateProfile() {
+    const { bio, name, username } = profileInfo;
+    await axios.put("/api/profile", {
+      bio,
+      name,
+      username,
+    });
+    setEditMode(false);
+  }
+
+  function cancel() {
+    setProfileInfo((prev) => {
+      const { bio, name, username } = originalUserInfo;
+      return { ...prev, bio, name, username };
+    });
+    setEditMode(false);
+  }
+
+  const isMyProfile = profileInfo?._id === userInfo?._id;
+
   return (
     <Layout>
       {!!profileInfo && (
@@ -47,35 +70,115 @@ export default function UserPage() {
             <TopNavLink title={profileInfo.name} />
           </div>
           <Cover
-            editable={true}
             src={profileInfo.cover}
+            editable={isMyProfile}
             onChange={(src) => updateUserImage("cover", src)}
           />
-          <div className="flex justify-between ">
+          <div className="flex justify-between">
             <div className="ml-5 relative">
               <div className="absolute -top-12 border-4 rounded-full border-black overflow-hidden">
                 <Avatar
                   big
                   src={profileInfo.image}
-                  editable={true}
+                  editable={isMyProfile}
                   onChange={(src) => updateUserImage("image", src)}
                 />
               </div>
             </div>
             <div className="p-2">
-              <button className="bg-twitterBlue text-white py-2 px-5 rounded-full">
-                Follow
-              </button>
+              {!isMyProfile && (
+                <button className="bg-twitterBlue text-white py-2 px-5 rounded-full">
+                  Follow
+                </button>
+              )}
+              {isMyProfile && (
+                <div>
+                  {!editMode && (
+                    <button
+                      onClick={() => setEditMode(true)}
+                      className="bg-twitterBlue text-white py-2 px-5 rounded-full"
+                    >
+                      Edit profile
+                    </button>
+                  )}
+                  {editMode && (
+                    <div>
+                      <button
+                        onClick={() => cancel()}
+                        className="bg-twitterWhite text-black py-2 px-5 rounded-full mr-2"
+                      >
+                        Cancel
+                      </button>
+                      <button
+                        onClick={() => updateProfile()}
+                        className="bg-twitterBlue text-white py-2 px-5 rounded-full"
+                      >
+                        Save profile
+                      </button>
+                    </div>
+                  )}
+                </div>
+              )}
             </div>
           </div>
           <div className="px-5 mt-2">
-            <h1 className="font-bold text-xl leading-5">{profileInfo.name}</h1>
-            <h2 className="text-twitterLightGray text-sm">
-              @{profileInfo.username}
-            </h2>
-            <div className="text-sm mt-2 mb-2">
-              Lorem ipsum dolor sit amet consectetur adipisicing.
-            </div>
+            {!editMode && (
+              <h1 className="font-bold text-xl leading-5">
+                {profileInfo.name}
+              </h1>
+            )}
+            {editMode && (
+              <div>
+                <input
+                  type="text"
+                  value={profileInfo.name}
+                  onChange={(ev) =>
+                    setProfileInfo((prev) => ({
+                      ...prev,
+                      name: ev.target.value,
+                    }))
+                  }
+                  className="bg-twitterBorder p-2 mb-2 rounded-full"
+                />
+              </div>
+            )}
+            {!editMode && (
+              <h2 className="text-twitterLightGray text-sm">
+                @{profileInfo.username}
+              </h2>
+            )}
+            {editMode && (
+              <div>
+                <input
+                  type="text"
+                  value={profileInfo.username}
+                  onChange={(ev) =>
+                    setProfileInfo((prev) => ({
+                      ...prev,
+                      username: ev.target.value,
+                    }))
+                  }
+                  className="bg-twitterBorder p-2 mb-2 rounded-full"
+                />
+              </div>
+            )}
+            {!editMode && (
+              <div className="text-sm mt-2 mb-2">{profileInfo.bio}</div>
+            )}
+            {editMode && (
+              <div>
+                <textarea
+                  value={profileInfo.bio}
+                  onChange={(ev) =>
+                    setProfileInfo((prev) => ({
+                      ...prev,
+                      bio: ev.target.value,
+                    }))
+                  }
+                  className="bg-twitterBorder p-2 mb-2 rounded-2xl w-full block"
+                />
+              </div>
+            )}
           </div>
         </div>
       )}
